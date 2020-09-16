607d75baaf4af5977d27b46aa090a19bc532d737
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-09-15 22:01:58 -0300

    Initial Commit

---
+++ MyGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGames
+//
+//  Created by Vinícius Tinajero Salomão on 15/09/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
